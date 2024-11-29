677eeadbe7497deba242a054be71ff897338eb3e
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-29 16:02:28 +0100

    ADD accordion logic

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,15 +1,25 @@
 import style from './Accordion.module.css';
 import languages from '../../assets/languages.js';
+import { useState } from 'react';
 
 export default function Accordion() {
+    const [activeId, setActiveId] = useState();
+
+    function handleToggle(id) {
+        setActiveId(activeId === id ? null : id);
+    }
+
     return (
         <div>
             <ul>
                 {languages.map((language) => (
                     <AccordionItem
                         key={language.id}
+                        id={language.id}
                         title={language.title}
                         description={language.description}
+                        active={activeId === language.id ? true : false}
+                        onClick={handleToggle}
                     />
                 ))}
             </ul>
@@ -17,11 +27,11 @@ export default function Accordion() {
     );
 }
 
-function AccordionItem({ title, description }) {
+function AccordionItem({ id, title, description, active, onClick }) {
     return (
         <li>
-            <h3>{title}</h3>
-            <p>{description}</p>
+            <h3 onClick={() => onClick(id)}>{title}</h3>
+            {active && <p>{description}</p>}
         </li>
     );
 }
